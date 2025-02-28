146deac0ba379e1c1cb058cd87a0a1b2366d0289
1obi <113096462+1obi@users.noreply.github.com>
2025-02-28 15:09:58 +0100

    Test

--- src/FishTank06.java
+++ src/FishTank06.java
@@ -1,4 +1,9 @@
+import java.time.LocalDateTime;
+import java.util.Scanner;
 public class FishTank06 {
 
     //Sådan. Det virker. Go gruppe 06
+    private String lastWaterChange;
+    private String waterQuality;
+
 }
